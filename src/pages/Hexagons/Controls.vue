341e980e4eb486729f8f2aecb774fec0f8c30458
pburkett <pburkett636@gmail.com>
2022-08-13 17:32:39 -0600

    collapsable controls and cheap max height aniamtion

--- src/pages/Hexagons/Controls.vue
+++ src/pages/Hexagons/Controls.vue
@@ -1,45 +1,80 @@
 <template>
   <div class="controls">
-    <div style="display: flex; width: 100%; justify-content: space-between; font-weight: 700;font-size:18px">
-      <h5>Most</h5>
-      <h5>Least</h5>
-    </div>
-    <div style="display: flex; box-shadow: 0px 2px 6px 2px rgba(0,0,0,.3);border-radius: 6px 6px 20px 20px;margin-left:20px;margin-right:20px">
+    <div
+      :style="{ maxHeight: isClosed ? '0px' : '1000px' }"
+      style="overflow: hidden; transition: max-height 2s ease"
+    >
       <div
-        v-for="color in colors"
-        :key="color"
-        :style="{ backgroundColor: '#' + color }"
-        class="color-button"
+        style="
+          display: flex;
+          width: 100%;
+          justify-content: space-between;
+          font-weight: 700;
+          font-size: 18px;
+        "
       >
-        <p
-          :style="{ color: invertColor(color) }"
-          style="font-weight: 600"
-        >
-          #{{ color }}
-        </p>
+        <h5>Most</h5>
+        <h5>Least</h5>
       </div>
+      <div
+        style="
+          display: flex;
+          box-shadow: 0px 2px 6px 2px rgba(0, 0, 0, 0.3);
+          border-radius: 6px 6px 20px 20px;
+          margin-left: 20px;
+          margin-right: 20px;
+        "
+      >
+        <div
+          v-for="color in colors"
+          :key="color"
+          :style="{ backgroundColor: '#' + color }"
+          class="color-button"
+        >
+          <p :style="{ color: invertColor(color) }" style="font-weight: 600">
+            #{{ color }}
+          </p>
+        </div>
+      </div>
+      <input
+        style="margin-top: 20px"
+        @change="pickColor($event)"
+        type="color"
+      />
     </div>
 
-    <input style="margin-top: 20px" @change="pickColor($event)" type="color" />
+      <div style="display: flex; justify-content: end"  @click="isClosed = !isClosed">
+          <ion-icon
+            name="caret-down-outline"
+            class="md icon-large hydrated"
+            :class="{ 'rotate-180': !isClosed, 'rotate-0': isClosed }"
+            style="
+              transition: transform 1s ease;
+              visibility: visible !important;
+            "
+            size="large"
+          ></ion-icon>
+      </div>
   </div>
 </template>
 
 <script>
-import colorService from "./ColorService"
+import colorService from "./ColorService";
 export default {
   props: ["colors"],
   data() {
     return {
       newColor: "",
+      isClosed: false,
     };
   },
   methods: {
-    pickColor(e){
-        this.$emit('addColor', e.target.value.slice(1))
+    pickColor(e) {
+      this.$emit("addColor", e.target.value.slice(1));
     },
-    invertColor(hex){
-        colorService.invertColor(hex,true)
-    }
+    invertColor(hex) {
+      colorService.invertColor(hex, true);
+    },
   },
 };
 </script>
@@ -53,9 +88,9 @@ export default {
   z-index: 10;
   left: 50%;
   transform: translateX(-50%);
-  box-shadow: 0px 8px 8px 5px rgba(0,0,0,.3);
-  top:0px;
-  margin-top:8px;
+  box-shadow: 0px 8px 8px 5px rgba(0, 0, 0, 0.3);
+  top: 0px;
+  margin-top: 8px;
 }
 .color-button {
   padding: 0px 5px 20px 5px;
@@ -68,5 +103,11 @@ export default {
 }
 .color-button:only-child {
   border-radius: 6px 6px 20px 20px !important;
+}
+.rotate-180 {
+  transform: rotateZ(180deg);
+}
+.rotate-0 {
+  transform: rotateZ(0deg);
 }
 </style>
