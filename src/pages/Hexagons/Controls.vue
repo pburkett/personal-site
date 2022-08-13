a56088567ed2f39ac339c35605b433f7178863b2
pburkett <pburkett636@gmail.com>
2022-08-13 17:11:41 -0600

    control paddng and shadows

--- src/pages/Hexagons/Controls.vue
+++ src/pages/Hexagons/Controls.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="controls">
-    <div style="display: flex; width: 100%; justify-content: space-between">
+    <div style="display: flex; width: 100%; justify-content: space-between; font-weight: 700;font-size:18px">
       <h5>Most</h5>
       <h5>Least</h5>
     </div>
-    <div style="display: flex">
+    <div style="display: flex; box-shadow: 0px 2px 6px 2px rgba(0,0,0,.3);border-radius: 6px 6px 20px 20px;margin-left:20px;margin-right:20px">
       <div
         v-for="color in colors"
         :key="color"
@@ -20,7 +20,7 @@
       </div>
     </div>
 
-    <input @change="pickColor($event)" type="color" />
+    <input style="margin-top: 20px" @change="pickColor($event)" type="color" />
   </div>
 </template>
 
@@ -53,17 +53,20 @@ export default {
   z-index: 10;
   left: 50%;
   transform: translateX(-50%);
+  box-shadow: 0px 8px 8px 5px rgba(0,0,0,.3);
+  top:0px;
+  margin-top:8px;
 }
 .color-button {
   padding: 0px 5px 20px 5px;
 }
 .color-button:last-child {
-  border-radius: 0px 10px 20px 0px;
+  border-radius: 0px 6px 20px 0px;
 }
 .color-button:first-child {
-  border-radius: 10px 0px 0px 20px;
+  border-radius: 6px 0px 0px 20px;
 }
 .color-button:only-child {
-  border-radius: 10px 10px 20px 20px !important;
+  border-radius: 6px 6px 20px 20px !important;
 }
 </style>
