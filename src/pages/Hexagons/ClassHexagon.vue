c9becaad0e9bc8a4f8a33a8ce79b7f375187a40a
pburkett <pburkett636@gmail.com>
2022-08-13 15:25:41 -0600

    select colors from a list

---
+++ src/pages/Hexagons/ClassHexagon.vue
@@ -0,0 +1,54 @@
+<template>
+  <div style="position: relative">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      :width="54.324000000000005 * sizeScalar"
+      :height="62.856 * sizeScalar"
+      :viewBox="`0 0 362.16 419.04`"
+    >
+      <path
+      :style="{'transitionDuration':speed * 2 + 'ms' }"
+        class="hex-path"
+        transform="matrix(0 0.72 -0.72 0 360.72 2.2111206642071)"
+        stroke="#ffffff"
+        :fill="'#' + color"
+        stroke-width="20"
+        stroke-linecap="butt"
+        stroke-linejoin="miter"
+        stroke-miterlimit="2"
+        d="M575.929 249.385L431.947 498.769L143.982 498.769L0 249.385L143.982 0L431.947 0L575.929 249.385Z"
+      ></path>
+    </svg>
+  </div>
+</template>
+
+<script>
+import { thisTypeAnnotation } from "@babel/types";
+import perlin from "../../perlin.js";
+export default {
+  props: ["x", "y", "colors", 'globalOffset', 'speed'],
+  data() {
+    return { color: "", sizeScalar: 1,  };
+  },
+  methods: {
+    setColor() {
+      let noise = Math.abs(perlin.get(this.x + this.globalOffset, this.y + this.globalOffset)) * 1.35;
+      let index = Math.floor(noise * this.colors.length);
+      this.color = this.colors[index];
+      if (index >= 8) console.log(index);
+    },
+  },
+  mounted() {
+    this.setColor();
+    setInterval(() => {
+      this.setColor();
+    }, this.speed);
+  },
+};
+</script>
+
+<style>
+.hex-path {
+  transition: fill linear;
+}
+</style>
--- src/pages/Hexagons/Hexagons.vue
+++ src/pages/Hexagons/Hexagons.vue
@@ -1,29 +1,60 @@
 <template>
-  <div style="margin-left: -50px; ">
+  <div style="height: 50vh; overflow: hidden; width: 100vw">
     <div
       class="row"
       :class="{ 'offset-row': i % 2 == 0 }"
-      style="display: flex"
+      style="display: flex; justify-content: center"
       v-for="i in 20"
       :key="i"
     >
-      <Hexagon v-for="n in 40" :key="n"  :x="n" :y="i" :global-offset="globalOffset" />
+      <!-- <Hexagon v-for="n in 40" :key="n"  :x="n" :y="i" :global-offset="globalOffset" /> -->
+      <ClassHexagon
+        v-for="n in 40"
+        :key="n"
+        :x="n"
+        :y="i"
+        :colors="colors"
+        :global-offset="globalOffset"
+        :speed="speed"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import Hexagon from "./Hexagon.vue";
+// import Hexagon from "./Hexagon.vue";
+import ClassHexagon from "./ClassHexagon.vue";
+import perlin from "../../perlin.js";
+
 export default {
   name: "Hexagons",
-  components: { Hexagon },
+  components: {
+    // Hexagon,
+    ClassHexagon,
+  },
   data() {
-    return { color: "#0000ff", globalOffset: Math.random() };
+    return {
+      globalOffset: Math.random(),
+      colors: [
+        "2a4858",
+        "215d6e",
+        "08737f",
+        "00898a",
+        "089f8f",
+        "39b48e",
+        "64c987",
+        "92dc7e",
+        "c4ec74",
+        "fafa6e",
+      ],
+      speed: 1000,
+    };
   },
   mounted() {
-        setInterval(() => {
-            this.globalOffset += 1
-    }, 1000);
+    perlin.seed();
+    setInterval(() => {
+      this.globalOffset += 0.1;
+    }, this.speed);
   },
 };
 </script>
